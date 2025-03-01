f76d0432fef614ec90fa69703bcff323c5be2a98
devCarlJoseph <carljosephsumagang17@gmail.com>
2025-03-01 17:55:40 +0800

    new changes

--- assets/product.js
+++ assets/product.js
@@ -1,7 +1,9 @@
 let chair = document.getElementById("chair");
 let sofa = document.getElementById("sofa");
+let lamp = document.getElementById("lamp");
 let chairButton = document.getElementsByClassName("chairb");
 let sofaButton = document.getElementsByClassName("sofab")
+let lampButton = document.getElementsByClassName("lampb")
 
 chairButton[0].onclick = function() {
     for (button of chairButton) {
@@ -67,8 +69,7 @@ chairButton[3].onclick = function() {
     chairThree.src = "images/chairs/Vero Lounge.jpg ";
     chairFour.src = "images/chairs/Zara Chair.jpg ";
 }
-
-// Sofa
+//sofa
 sofaButton[0].onclick = function () {
     for (sButton of sofaButton) {
         sButton.classList.remove("active");
@@ -112,4 +113,121 @@ sofaButton[3].onclick = function () {
     sofaTwo.src = "images/sofa/Vera Sofa.jpg"; 
     sofaThree.src = "images/sofa/Willow Seat.jpg"; 
     sofaFour.src = "images/sofa/Zenith Sofa.jpg"; 
+}
+//lamp
+
+
+
+
+
+
+
+
+
+
+const cartIcon = document.querySelector("#cart-icon");
+const cart = document.querySelector(".cart");
+const cartClose = document.querySelector("#cart-close");
+cartIcon.addEventListener("click", () => cart.classList.add("active"));
+cartClose.addEventListener("click", () => cart.classList.remove("active"));
+
+const addCartButtons = document.querySelectorAll(".add-cart");
+addCartButtons.forEach(button => {
+    button.addEventListener("click", event => {
+        const productBox = event.target.closest(".main-chair");
+        addToCart(productBox);
+    });
+});
+
+const cartContent = document.querySelector(".cart-content");
+const addToCart = productBox => {
+    const productImgSrc = productBox.querySelector("img").src;
+    const productTitle = productBox.querySelector(".product-title").textContent;
+    const productPrice = productBox.querySelector(".product-price").textContent;
+
+    const cartItems = cartContent.querySelectorAll(".cart-product-title");
+    for (let item of cartItems) {
+        if (item.textContent === productTitle) {
+            alert("Naana Boy.");
+            return;
+        }
+    }
+
+    const cartBox = document.createElement("div");
+    cartBox.classList.add("cart-box");
+    cartBox.innerHTML = `
+            <div class="cart-box">
+            <img src="${productImgSrc}" class="cart-image">
+            <div class="cart-details">
+                <h2 class="cart-product-title">${productTitle}</h2>
+                <span class="cart-price">${productPrice}</span>
+                <div class="cart-quantity">
+                    <button id="decrement">-</button>
+                    <span class="number">1</span>
+                    <button id="increment">+</button>
+                </div>
+            </div>
+            <i class="fa-solid fa-trash cart-remove"></i>
+        `;
+
+        cartContent.appendChild(cartBox);
+
+        cartBox.querySelector(".cart-remove").addEventListener("click", () => {
+            cartBox.remove();
+
+            updateCartCount(-1);
+
+            updateTotalPrice();
+        });
+
+        cartBox.querySelector(".cart-quantity").addEventListener("click", event => {
+            const numberElement = cartBox.querySelector(".number");
+            const decrementButton = cartBox.querySelector("#decrement");
+            let quantity = numberElement.textContent;
+
+            if (event.target.id === "decrement" && quantity > 1) {
+                quantity--;
+                if (quantity === 1) {
+                    decrementButton.style.color = "#999";
+                }
+            } else if (event.target.id === "increment") {
+                quantity++;
+                decrementButton.style.color = "#333";
+            }
+
+            numberElement.textContent = quantity;
+
+            updateTotalPrice();
+        });
+
+        updateCartCount(1);
+
+        updateTotalPrice();
+};
+
+const updateTotalPrice = () => {
+    const totalPriceElement = document.querySelector(".total-price");
+    const cartBoxes = cartContent.querySelectorAll(".cart-box");
+    let total = 0;
+    cartBoxes.forEach(cartBox => {
+        const priceElement = cartBox.querySelector(".cart-price");
+        const quantityElement = cart.querySelector(".number");
+        const price = priceElement.textContent.replace("P", "");
+        const quantity = quantityElement.textContent;
+        total += price * quantity;
+    });
+    totalPriceElement.textContent = `P${total}`;
+};
+
+let cartItemCount = 0;
+const updateCartCount = change => {
+    const cartItemCountBadge = document.querySelector(".cart-items-count");
+    cartItemCount += change;
+    if (cartItemCount > 0) {
+        cartItemCountBadge.style.visibility = "visible";
+        cartItemCountBadge.textContent = cartItemCount;
+    } else {
+        cartItemCountBadge.style.visibility = "hidden";
+        cartItemCountBadge.textContent = "";
+    }
 }
